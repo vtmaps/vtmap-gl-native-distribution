80ef922dc0f09397706729c16efe7e77b93005fa
QuanDHA <quandha@viettel.com.vn>
2026-06-03 10:13:09 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "VTMap",
 //            url: "https://github.com/maplibre/maplibre-native/releases/download/ios-v6.26.1/MapLibre.dynamic.xcframework.zip",
-            url: "https://github.com/vtmaps/vtmap-gl-native-distribution/releases/download/6.26.1/VTMap.dynamic.xcframework.zip",
-            checksum: "ba430873dfdf6fd95065ac90f52a0fe3b496234f545899773229560a4c0387b4")
+            url: "https://arcgis.vgis.vn/files/VTMap.dynamic.xcframework.zip",
+            checksum: "https://arcgis.vgis.vn/files/VTMap.dynamic.xcframework.zip")
     ]
 )
